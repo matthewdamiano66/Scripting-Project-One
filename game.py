282f9bf0bac378fdbb28c7eab8e0c2d05c6e48fb
matthewdamiano66 <matthew.damiano610@gmail.com>
2022-09-29 16:34:09 -0400

    update to game, added ability to know how many items you possess in the inventory list using len(inventory)

--- game.py
+++ game.py
@@ -1,22 +1,24 @@
+# rooms = 0            1           2           3         4           5           6           7
 rooms = ['entrance', 'hallway', 'bedroom', 'basement', 'stairwell', 'Kitchen', 'trap room', 'exit']
-items = ['flashlight', 'mirror', 'key', 'camera', 'cellphone', 'net']
+# items =      0           1        2      3          4          5
+items = ['flashlight', 'mirror', 'keys', 'camera', 'cellphone', 'net']
 # I should use this method instead but who the fuck cares lmao
 # directions = ['north','south','east','west']
 visited = []
 inventory = []
 villain = 'ghost'
 line = "---------------------------------------------------------------------------------------------------------------"
+# initial user data
 name = input("Enter your name:")
 data = input(name.lower().strip() + " " + "Do you want to play a game ? (Yes/No?)")
 # ----------------------------------------------------------------------------------------------------------------------
-# print(visited)
 # Game starts
 if data.lower().strip() == "yes":
     visited.append(rooms[0])
     print("Collect all 6 items to win \n")
     print(line)
-    print("For weeks you've heard stories of miissing people entering the haunted Arley home.\n"
-          "Following these stories you decide to investigae the home.\n")
+    print("For weeks you've heard stories of missing people entering the haunted Arley home.\n"
+          "Following these stories you decide to investigate the home.\n")
     print(line)
     data = input("You enter a dark and dingy home standing in the " + " " + rooms[
         0] + " " + "you hear a strange noise do you head toward the hallway or basement?")
@@ -29,30 +31,52 @@ else:
     print("Ok" + " " + name.lower().strip() + "," + " goodbye for now")
     # this is all working now
 # ----------------------------------------------------------------------------------------------------------------------
-    if data.strip().lower() == "hallway":
-        data = input(
-            "you've entered the hallway but hear foot steps you find a " + " " + items[
-                0] + " " + "where do you go next ? the Kitchen? or the stairwell?\n")
-        inventory.append(items[0])
-        visited.append(rooms[1])
-        print(visited)
-        print(inventory)
+if data.strip().lower() == "hallway":
+    data = input(
+        "you've entered the hallway but hear foot steps you find a " + " " + items[
+            0] + " " + "where do you go next ? the Kitchen? or the stairwell?\n")
+    inventory.append(items[0])
+    visited.append(rooms[1])
 # ----------------------------------------------------------------------------------------------------------------------
-        if data.strip().lower() == "kitchen":
-            print("Entering the kitchen you use your flashlight to look around,you find a" + str(
-                items[3]) + " " + "this may be helpful:")
-            print("You have found 2 out of 6 items !")
-            input("Hearing noises you rush out of the kitchen, where will you head next?")
-        inventory.append(items[3])
-        visited.append(rooms[5])
-        print(inventory)
-# ----------------------------------------------------------------------------------------------------------------------
-if data.strip().lower() == "stairwell":
-    print("Ascending the stairs with your flashlight, peering around for clues you suddenly rush back into the hallway")
-# ----------------------------------------------------------------------------------------------------------------------
-elif data.strip().lower() == "basement":
+# first possible game over not based on simply declining to play the game
+if data.strip().lower() == "basement":
     print("You feel a presence fast approaching, the" + " " + villain + " " + "approaches too fast to react\n")
     print(line)
     print("Game Over," + " " + name + " " + "was never seen again!")
     visited.append(rooms[3])
     print("You visited:" + str(visited))
+    # ----------------------------------------------------------------------------------------------------------------------
+    if data.strip().lower() == "kitchen":
+        print("Entering the kitchen you use your flashlight to look around,you find a" + str(
+            items[3]) + " " + "this may be helpful:")
+        inventory.append(items[3])
+        print("You have found "+str(len(inventory))+" out of 6 items !")
+        data = input("Hearing noises you rush out of the kitchen, where will you head next?")
+        visited.append(rooms[5])
+# ----------------------------------------------------------------------------------------------------------------------
+if data.strip().lower() == "stairwell":
+    visited.append(rooms[4])
+    print("You climb the stairs, noticing the state of the hallway you find yourself in you descend quickly back to "
+          "whence you came.\n")
+    print("Looking around quickly you find a set of keys along the stairs where could they lead ?\n")
+    inventory.append(items[2])
+    print("You have found " + str(len(inventory)) + " out of 6 items !")
+    print(line)
+    data = input("Back where you started where will you head next ?\n to the kitchen? or downward to the basement?")
+    if data == "basement":
+        inventory.append(items[1])
+        print("While is the basement using your flashlight you find a mirror, how could this be helpful you think?\n"
+              "")
+    if data.strip().lower() == "kitchen":
+        print("Entering the kitchen you use your flashlight to look around,you find a" + str(
+            items[3]) + " " + "this may be helpful:")
+        inventory.append(items[3])
+        print("You have found " + str(len(inventory)) + " out of 6 items !")
+        data = input("idk man plot is hard")
+        visited.append(rooms[5])
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+
+
+# ----------------------------------------------------------------------------------------------------------------------
